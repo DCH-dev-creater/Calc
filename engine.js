7093ad8efad43496677d41eb62ef198529c31892
DCH <chernykh_dv@citrus.com.ua>
2025-05-16 22:17:56 +0300

    Implement initial calculator functionality with event listeners for mouse interactions

---
+++ engine.js
@@ -0,0 +1,47 @@
+window.addEventListener('load', function() {
+
+    const calculator = document.getElementById('calculator');
+    const display = document.getElementById('display');
+    const symbols = document.getElementById('symbols');
+    const memorySymbol = document.getElementById('memory-symbol');
+    const buttonPanel = document.getElementById('button-panel');
+
+    calculator.addEventListener('mousemove', function(event) {
+        if (isButton(event)) return;
+        if (event.buttons === 1) {
+            calculator.style.left = (calculator.offsetLeft+event.movementX) + 'px';
+            calculator.style.top = (calculator.offsetTop+event.movementY) + 'px';
+        }
+    });
+
+    buttonPanel.addEventListener('mousedown', function(event) {
+        if (!isButton(event)) return;
+        event.target.classList.add('pressed');
+    });
+
+    buttonPanel.addEventListener('mouseup', function(event) {
+        if (!isButton(event)) return;
+        event.target.classList.remove('pressed');
+    });
+    
+
+    // Ganeral Function
+    buttonPanel.addEventListener('click', function(event) {
+        if (!isButton(event)) {
+            return; 
+        }
+
+        
+
+    })
+
+
+
+
+    function isButton(e) {
+        return e.target.hasAttribute('data-button');
+    }
+
+
+
+});
