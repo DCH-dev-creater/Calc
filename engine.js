29df5f02deecffdb636ce8503d7f1832b8dc37e5
DCH <chernykh_dv@citrus.com.ua>
2025-05-23 22:26:39 +0300

    Refactor state management in buttonPanel event listener and enhance display logic

--- engine.js
+++ engine.js
@@ -4,8 +4,8 @@ const DOM = init();
 
 var state = {
     value: 0,
-    memoryValue: 0,
     displayValue: '0',
+    operator: null,
 };
 
 DOM.buttonPanel.addEventListener('click', function(event) {
@@ -25,8 +25,11 @@ DOM.buttonPanel.addEventListener('click', function(event) {
         case '8':
         case '9':
         case '0':
+
             state.displayValue = addDigit(state.displayValue, symbol);
             showToDisplay(state.displayValue, DOM.display);
+            console.log('display: ' + state.displayValue + ' value: ' + state.value + ' operator: ' + state.operator);
+            
             return;
 
         case 'dot':
@@ -36,7 +39,15 @@ DOM.buttonPanel.addEventListener('click', function(event) {
             return;
 
         case 'plus_minus':
-            console.log('plus_minus');
+            if (state.displayValue === '0') return;
+
+            if (state.displayValue.startsWith('-')) {
+                state.displayValue = state.displayValue.replace('-', '');
+            } else {
+                state.displayValue = '-' + state.displayValue;
+            }
+            showToDisplay(state.displayValue, DOM.display);
+            
             return;
 
         case 'ce':
@@ -45,7 +56,26 @@ DOM.buttonPanel.addEventListener('click', function(event) {
             state.value = 0;
             showToDisplay(state.displayValue, DOM.display);
             return;
+
+        case 'plus':
+            state.operator = '+';
+            state.value = Number(state.displayValue);
+            state.displayValue = '0';
+            
+            return;
+
+        case 'equals':
+            if (state.operator === null) return;
+
+            if (state.operator === '+') {
+                state.value += Number(state.displayValue);
+            }
+
+            showToDisplay(state.value, DOM.display);
+
+            return;
     }
 
 
 });
+
